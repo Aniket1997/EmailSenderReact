33bca744e8a26089a0cb8d2c22663910103a8447
aniKET_Paul <16riju@gmail.com>
2023-12-30 03:23:49 +0530

    Addfrontend 4

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -98,7 +98,7 @@ const handleEmailDelete = (email) => {
   
     console.log('Data to be sent:', dataSend);
   
-    const resValue = await fetch(`${baseUrl}/email/sendEmail`, {
+    const resValue = await fetch(`${baseUrl}/sendEmail`, {
       method: "POST",
       body: JSON.stringify(dataSend),
       headers: {
